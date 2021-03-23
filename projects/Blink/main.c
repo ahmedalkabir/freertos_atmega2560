7b76e31d06bd3ee55682254b34b1f44c0e2ec840
Ahmed Alkabir <korabekaali@outlook.com>
2021-03-24 00:36:09 +0200

    add the project to repository

---
+++ projects/Blink/main.c
@@ -0,0 +1,42 @@
+/*
+ * freertos.c
+ *
+ * Created: 2018/4/6 6:41:22
+ * Author : Pluto
+ */ 
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "croutine.h"
+
+#define task1_TASK_PRIORITY					( tskIDLE_PRIORITY + 1 )
+
+void vApplicationIdleHook( void );
+extern void vUARTCommandConsoleStart( uint16_t usStackSize, UBaseType_t uxPriority );
+
+void vTask1(void *pvParameters) 
+{
+	while (1) {
+		PORTB = ~PORTB;
+		vTaskDelay(2000);		
+	}
+}
+
+int main( void )
+{
+	DDRB = 0xff;
+	xTaskCreate(vTask1, "Task 1", configMINIMAL_STACK_SIZE, NULL,
+	task1_TASK_PRIORITY, NULL);
+
+	vTaskStartScheduler();
+	
+	return 0;
+}
+
+void vApplicationIdleHook( void )
+{
+	vCoRoutineSchedule();
+}
